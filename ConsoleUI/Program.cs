9935c085ff4d91e2f381e176575a278568acc501
Emrah <emrahdrsn@gmail.com>
2020-06-02 07:14:17 +0200

    Experience and Birthday Check
    In a single line checking both info

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,11 +13,18 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             List<Person> people = ListManager.LoadSampleData();
-            
+
             // .OrderBy returns a IEnumerable, That want us to write back To people List
             // now we are overwriting our people list with ordered list we can also write it to new list
             // here is  x represent a person, you are ordering them by their surname and writing down to list
-            people = people.OrderBy(x => x.LastName).ThenByDescending(x => x.YearsExperience ).ToList();
+            //      people = people.OrderBy(x => x.LastName).ThenByDescending(x => x.YearsExperience ).ToList();
+            //First we order the Last name then we took them and descend by years of experience
+
+            // This is another example is ::: repeating descending ThenBy it does the same thing as before order or descend
+            //      people = people.OrderByDescending(x => x.LastName).ThenBy(x => x.YearsExperience).ToList();
+
+            people = people.Where(x => x.YearsExperience > 5 && x.Birthday.Month == 8).ToList();  // If person x has more experience then 5 years and born month 8, add him to list
+
 
             foreach (var person in people)
             {
--- LinqLibrary/ListManager.cs
+++ LinqLibrary/ListManager.cs
@@ -12,13 +12,13 @@ namespace LinqLibrary
         {
             List<Person> output = new List<Person>();
 
-            output.Add(new Person { FirstName = " Emrah", LastName = " Durusu ", Birthday = Convert.ToDateTime("16/08/1969"), YearsExperience = Convert.ToInt32("8") });
-            output.Add(new Person { FirstName = " Savas", LastName = " Dursun ", Birthday = Convert.ToDateTime("16/08/1555"), YearsExperience = Convert.ToInt32("5") });
-            output.Add(new Person { FirstName = " Emira", LastName = " Ursula ", Birthday = Convert.ToDateTime("16/08/1999"), YearsExperience = Convert.ToInt32("9") });
+            output.Add(new Person { FirstName = " Emrah", LastName = " Durusu ", Birthday = Convert.ToDateTime("16/01/1969"), YearsExperience = Convert.ToInt32("8") });
+            output.Add(new Person { FirstName = " Savas", LastName = " Dursun ", Birthday = Convert.ToDateTime("16/08/1555"), YearsExperience = Convert.ToInt32("15") });
+            output.Add(new Person { FirstName = " Emira", LastName = " Ursula ", Birthday = Convert.ToDateTime("16/04/1999"), YearsExperience = Convert.ToInt32("9") });
             output.Add(new Person { FirstName = " Demir", LastName = " Domanl ", Birthday = Convert.ToDateTime("16/08/2003"), YearsExperience = Convert.ToInt32("3") });
             output.Add(new Person { FirstName = " Amira", LastName = " Durabl ", Birthday = Convert.ToDateTime("16/08/1209"), YearsExperience = Convert.ToInt32("19") });
-            output.Add(new Person { FirstName = " Demik", LastName = " Domanl ", Birthday = Convert.ToDateTime("16/08/2003"), YearsExperience = Convert.ToInt32("30") });
-            output.Add(new Person { FirstName = " Amira", LastName = " Durabl ", Birthday = Convert.ToDateTime("16/08/1209"), YearsExperience = Convert.ToInt32("10") });
+            output.Add(new Person { FirstName = " Demik", LastName = " Domanl ", Birthday = Convert.ToDateTime("16/05/2003"), YearsExperience = Convert.ToInt32("30") });
+            output.Add(new Person { FirstName = " Amira", LastName = " Durabl ", Birthday = Convert.ToDateTime("16/02/1209"), YearsExperience = Convert.ToInt32("10") });
             return output;
         }
     }
