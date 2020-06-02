b78daf7d381002f01c6435e4648504e4822ac345
Emrah <emrahdrsn@gmail.com>
2020-06-02 07:22:13 +0200

    Sum

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -17,19 +17,26 @@ namespace ConsoleUI
             // .OrderBy returns a IEnumerable, That want us to write back To people List
             // now we are overwriting our people list with ordered list we can also write it to new list
             // here is  x represent a person, you are ordering them by their surname and writing down to list
-            //      people = people.OrderBy(x => x.LastName).ThenByDescending(x => x.YearsExperience ).ToList();
-            //First we order the Last name then we took them and descend by years of experience
 
-            // This is another example is ::: repeating descending ThenBy it does the same thing as before order or descend
-            //      people = people.OrderByDescending(x => x.LastName).ThenBy(x => x.YearsExperience).ToList();
-
-            people = people.Where(x => x.YearsExperience > 5 && x.Birthday.Month == 8).ToList();  // If person x has more experience then 5 years and born month 8, add him to list
+            //            people = people.OrderBy(x => x.LastName).ThenByDescending(x => x.YearsExperience ).ToList();  //First we order the Last name then we took them and descend by years of experience
 
 
-            foreach (var person in people)
-            {
-                Console.WriteLine($"{person.FirstName} {person.LastName} ( {person.Birthday.ToShortDateString() } ) : Experience {person.YearsExperience}");
-            }
+            //            people = people.OrderByDescending(x => x.LastName).ThenBy(x => x.YearsExperience).ToList();   // repeating descending ThenBy it does the same thing as before order or descend
+
+            //            people = people.Where(x => x.YearsExperience > 5 && x.Birthday.Month == 8).ToList();           // If person x has more experience then 5 years and born month 8, add him to list
+
+
+            //foreach (var person in people)
+            //{
+            //    Console.WriteLine($"{person.FirstName} {person.LastName} ( {person.Birthday.ToShortDateString() } ) : Experience {person.YearsExperience}");
+            //}
+
+
+            int yearsTotal = people.Sum(x => x.YearsExperience); // Take each persons experience in people list. And sum up them
+            Console.WriteLine($"Total experience is =  {yearsTotal}");  // output =  94
+
+
+
             Console.ReadLine();
         }
     }
